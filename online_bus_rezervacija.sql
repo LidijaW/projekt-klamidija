2ed1813637a48a9a2074ab13cec1cabcd79658bc
Lidl <58550278+LidijaW@users.noreply.github.com>
2022-08-17 09:57:25 +0200

    Update online_bus_rezervacija.sql

--- online_bus_rezervacija.sql
+++ online_bus_rezervacija.sql
@@ -5,61 +5,61 @@ USE online_bus_rezervacija;
 CREATE TABLE bus(
 	id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	ime VARCHAR(32) NOT NULL,
-    broj_busa
+     	broj_busa
 	broj_sjedala_busa
-    bus_karta
-    tip_busa
-    kapacitet
-    raspored_putovanja_id
+   	bus_karta
+   	tip_busa
+        kapacitet
+        raspored_putovanja_id
 );
 
 CREATE TABLE booking(
 	id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    kupac_id,
-    rezervacija_id
-    karta_id,
-    datum,
-    opis,
-    nacin_placanja_id
+   	kupac_id,
+   	rezervacija_id
+    	karta_id,
+    	datum,
+    	opis,
+    	nacin_placanja_id
 	raspored_putovanja_id
 );
 
 CREATE TABLE kupac(
 	id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    ime VARCHAR(32) NOT NULL,
-    prezime VARCHAR(32) NOT NULL,
-    broj_telefona VARCHAR(16) NOT NULL UNIQUE,
+    	ime VARCHAR(32) NOT NULL,
+    	prezime VARCHAR(32) NOT NULL,
+    	broj_telefona VARCHAR(16) NOT NULL UNIQUE,
 	email VARCHAR(64) NOT NULL UNIQUE,
 
 );
 CREATE DATABASE rezervacija(
 	id,
-    kupac_id,
-    karta_id,
-    datum_rezervacije,
+    	kupac_id,
+    	karta_id,
+    	datum_rezervacije,
 
 );
 CREATE TABLE karta(
 	id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    rezervacija_id
-    ime_kupca
-    prezime-kupca
+    	rezervacija_id
+    	ime_kupca
+    	prezime-kupca
 	email_kupca
-    opis_karte
+    	opis_karte
 );
 
 CREATE TABLE vozac(
 	id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    ime
-    prezime
-    bus_id
-    broj_busa_id
-    booking_karta_id
+    	ime
+    	prezime
+    	bus_id
+    	broj_busa_id
+    	booking_karta_id
 );
 
 CREATE TABLE povratna_informacija(
 	kupac_id
-    ocjena_putovanja
+    	ocjena_putovanja
     
 
 
@@ -68,33 +68,33 @@ CREATE TABLE povratna_informacija(
 CREATE TABLE raspored_putovanja(
 	id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	bus_id
-    vozac_id
-    datum_putovanja
-    pocetak_mjesta_putovanja
+    	vozac_id
+    	datum_putovanja
+    	pocetak_mjesta_putovanja
 	destinacija
 );
 
 CREATE DATABASE nacin_placanja(
 	id,
-    booking_id,
-    kupac_id,
-    karticno_placanje_id
-    datum,
-    cijena
-    raspored putovanja_id
+    	booking_id,
+    	kupac_id,
+    	karticno_placanje_id
+    	datum,
+    	cijena
+    	raspored putovanja_id
 );
 CREATE DATABASE karticno_placanje(
 	id;
-    paypal,
-    mastercard,
-    visa,
-    american express,
-    maestro
+    	paypal,
+    	mastercard,
+    	visa,
+    	american express,
+    	maestro
 );
 CREATE DATABASE prtljaga(
 	id,
-    kupac_id
-    tezina_prtljage
-    cijena_prtljage
+    	kupac_id
+    	tezina_prtljage
+    	cijena_prtljage
 );	
 
